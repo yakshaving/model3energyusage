14ce2d5f0afaf4fe829afdbd9665e8b35198f6f9
yakshaving <ash.bhoopathy@gmail.com>
2018-09-27 12:58:24 -0700

    more

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <p>Thank you for your help with optimizing energy usage - I'll publish these results on r/TeslaMotors and the corresponding FB groups that data was collected from.  Questions or ideas for improvement? <a href="http://twitter.com/ashbhoopathy">Message me here</a></p>
+    <iframe className="airtable-embed" src="https://airtable.com/embed/shragCKg9uB4Xiz7Z?backgroundColor=red" frameBorder="0" onMouseWheel="" width="100%" height="533" style={{background: "transparent", border: "1px solid #eee"}} ></iframe>
+  </Layout>
+)
+
+export default IndexPage
